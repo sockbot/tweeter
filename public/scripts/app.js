b4c2e9c20fc0df40b42125ffad156cd7ba6824b3
Andy <github@sockbot.com>
2019-08-13 20:02:07 -0700

    complete tweet rendering code and function to cycle through array of tweet objects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,19 +5,59 @@
  */
 
 $(document).ready(function() {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-  // const timeAgo = function ()
+  // calculates the amount of time passed since timestamp (in milliseconds) and returns a string in human-readable format
+  const timePassed = function(timestamp) {
+    const timeNow = Date.now();
+    const millsPassed = timeNow - timestamp;
+    const secondsPassed = millsPassed / 1000;
+    const minutesPassed = secondsPassed / 60;
+    const hoursPassed = minutesPassed / 60;
+    const daysPassed = hoursPassed / 24;
+
+    if (daysPassed >= 180) {
+      return "180+ days ago";
+    }
+    if (2 <= daysPassed && daysPassed < 180) {
+      return `${Math.round(daysPassed)} days ago`;
+    }
+    if (2 <= hoursPassed && daysPassed < 2) {
+      return `${Math.round(hoursPassed)} hours ago`;
+    }
+    if (2 <= minutesPassed && hoursPassed < 2) {
+      return `${Math.round(minutesPassed)} minutes ago`;
+    }
+    if (2 <= secondsPassed && minutesPassed < 2) {
+      return `${Math.round(secondsPassed)} seconds ago`;
+    }
+    return "Just now";
+  };
+  
+  // turns a single tweet object into HTML
   const createTweetElement = function(tweetObj) {
     return `
     <article class="tweet-container">
@@ -29,14 +69,20 @@ $(document).ready(function() {
         ${tweetObj.content.text}
       </main>
       <footer class="tweet-footer">
-        <span>${Date(tweetObj.created_at)}</span>
+        <span>${timePassed(tweetObj.created_at)}</span>
         <span class="tweet-icons"><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></span>
       </footer>
     </article>
     `;
   };
 
-  const $tweet = createTweetElement(tweetData);
-  // console.log($tweet);
-  // $('main.container').append($tweet);
+  // renders an array of tweet objects
+  const renderTweets = function(tweetObjArr) {
+    for (const obj of tweetObjArr) {
+      const $tweet = createTweetElement(obj);
+      $('main.container').append($tweet);
+    }
+  };
+
+  renderTweets(data);
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   const maxTweetLength = 140;
+  
   $('.new-tweet > form > textarea').on('keyup', function() {
     const tweetChars = $(this).val().length;
     const remainingChars = maxTweetLength - tweetChars;
     const counter = $(this).siblings('span.counter');
+    
     counter.text(remainingChars);
+
     if (remainingChars < 0) {
       counter.addClass('red');
     } else {
