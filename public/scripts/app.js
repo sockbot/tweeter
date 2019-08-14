41be89e6fbc5676fc8fe0a5fc286ccf6b965f3d6
Andy <github@sockbot.com>
2019-08-13 17:11:30 -0700

    implemented hover states for tweet articles

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  $('article.tweet-container').hover(function() {
+    const tweetContainer = $(this);
+    const tweetHandle = $(this).find('.tweet-handle');
+    tweetContainer.addClass('hover');
+    tweetHandle.addClass('hover');
+  }, function() {
+    const tweetContainer = $(this);
+    const tweetHandle = $(this).find('.tweet-handle');
+    tweetContainer.removeClass('hover');
+    tweetHandle.removeClass('hover');
+  })
+})
